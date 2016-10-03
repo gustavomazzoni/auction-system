662faaba097aeb21e63ccd3d139a631de381f7cd
Gustavo Mazzoni <gustavo.mazzoni@gmail.com>
2016-10-03 11:13:36 -0300

    Add configuration for MySQL on heroku.

---
+++ server/DB.sql
@@ -0,0 +1,16 @@
+
+-- ----------------------------
+-- Create Table structures
+-- ----------------------------
+CREATE TABLE IF NOT EXISTS `users` (`username` VARCHAR(50) NOT NULL, PRIMARY KEY (`username`));
+CREATE TABLE IF NOT EXISTS `players` (`username` VARCHAR(50) NOT NULL, `coins` INT(8) NOT NULL, PRIMARY KEY (`username`));
+CREATE TABLE IF NOT EXISTS `items` (`id` INT(8) NOT NULL AUTO_INCREMENT, `name` VARCHAR(50) NOT NULL, `avatar` VARCHAR(50), PRIMARY KEY (`id`));
+CREATE TABLE IF NOT EXISTS `players_items` (`id` INT(8) NOT NULL AUTO_INCREMENT, `username` VARCHAR(50) NOT NULL, `id_item` INT(8) NOT NULL, `quantity` INT(8) NOT NULL, PRIMARY KEY (`id`), CONSTRAINT fk_player FOREIGN KEY (`username`) REFERENCES players(`username`), CONSTRAINT fk_item FOREIGN KEY (`id_item`) REFERENCES items(`id`));
+CREATE TABLE IF NOT EXISTS `auctions` (`id` INT(8) NOT NULL AUTO_INCREMENT, `seller_username` VARCHAR(50) NOT NULL, `id_item` INT(8) NOT NULL, `quantity` INT(8) NOT NULL, `minimum_bid` INT(8) NOT NULL, `status` VARCHAR(10) NOT NULL, `winner_bid` INT(8), `winner_username` VARCHAR(50), PRIMARY KEY (`id`), CONSTRAINT fk_seller FOREIGN KEY (`seller_username`) REFERENCES players(`username`), CONSTRAINT fk_winner FOREIGN KEY (`winner_username`) REFERENCES players(`username`), CONSTRAINT fk_itemm FOREIGN KEY (`id_item`) REFERENCES items(`id`));
+
+-- ----------------------------
+-- Records of items
+-- ----------------------------
+INSERT INTO `items` (name, avatar) VALUES ('Bread','assets/bread.png');
+INSERT INTO `items` (name, avatar) VALUES ('Carrot','assets/carrot.png');
+INSERT INTO `items` (name, avatar) VALUES ('Diamond','assets/diamond.png');
--- server/config.js
+++ server/config.js
@@ -2,7 +2,7 @@ var path = require('path');
 
 module.exports = {
   database: {                                           // The connection object of our the MySQL DB server
-    connectionObj: {
+    connectionObj: process.env.CLEARDB_DATABASE_URL || {
       connectionLimit : 50,
       host     : 'localhost',
       user     : 'auction',
@@ -11,10 +11,10 @@ module.exports = {
     }
   },
   server: {
-    listenPort: 3000,                                   // The port on which the server is to listen (means that the app is at http://localhost:3000 for instance)
-    staticFolder: path.resolve(__dirname, '../client/build'),  // The folder that contains the application files (note that the files are in a different repository) - relative to this file
-    staticUrl: '/static',                               // The base url from which we serve static files (such as js, css and images)
-    apiV1Url: '/api/v1',                                // The base url from which we serve API v1 resources
-    cookieSecret: 'auction-system'                      // The secret for encrypting the cookie
+    listenPort: process.env.PORT || 3000,                      // The port on which the server is to listen (means that the app is at http://localhost:3000 for instance)
+    staticFolder: path.resolve(__dirname, '../client/bin'),  // The folder that contains the application files (note that the files are in a different repository) - relative to this file
+    staticUrl: '/static',                                      // The base url from which we serve static files (such as js, css and images)
+    apiV1Url: '/api/v1',                                       // The base url from which we serve API v1 resources
+    cookieSecret: 'auction-system'                             // The secret for encrypting the cookie
   }
 };
